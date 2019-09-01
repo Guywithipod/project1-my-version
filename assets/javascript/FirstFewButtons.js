a5f9d8eace6497cbc16323fcee340d82238a544d
Anthony Dominguez <Anthonydv7@outlook.com>
2019-09-01 10:28:24 -0500

    ingred is almost working with flex box

--- assets/javascript/FirstFewButtons.js
+++ assets/javascript/FirstFewButtons.js
@@ -1,5 +1,11 @@
 
+const getIngredients = obj => {
+    const re = 'strIngredient';
 
+    const keys = Object.keys(obj).filter(key => key.indexOf(re) > -1);
+    return keys.map(key => obj[key]).filter(ingred => ingred !== '');
+
+}
 document.querySelectorAll("button").forEach(function (node) {
 
     node.addEventListener("click", function () {
@@ -13,40 +19,37 @@ document.querySelectorAll("button").forEach(function (node) {
         }).then(function (responseJson) {
 
             const results = responseJson;
-
+            document.getElementById("drink-names").innerHTML = '';
+        
+            let wrapper;
             for (let i = 0; i < 3; i++) {
-
+                const ingredients = getIngredients(results.drinks[i]);
+                console.log(ingredients);
+                wrapper = document.createElement('div');
+                wrapper.classList.add('drink-wrapper');
                 const cockTail = results.drinks[i].strDrink;
                 const recipeInstructions = results.drinks[i].strInstructions;
                 const imgURL = responseJson.drinks[i].strDrinkThumb;
-
-                const drinkDiv = document.createElement("div");
-                drinkDiv.setAttribute("id", "container");
-
-                const recipeContainerDiv = document.createElement("div");
-                recipeContainerDiv.setAttribute("id", "instructions-container");
-
-                const imageContainerDiv = document.createElement("div");
-                imageContainerDiv.setAttribute("id", "image-container");
-        
-
+                const nameRecipeWrapper = document.createElement('div');
+                nameRecipeWrapper.classList.add('name-recipe-wrapper');
                 const cockTailDiv = document.createElement("p");
                 cockTailDiv.setAttribute("id", "name");
                 cockTailDiv.innerHTML = cockTail;
-
+                
                 const imageDiv = document.createElement("img");
                 imageDiv.setAttribute("src", imgURL);
                 imageDiv.innerHTML = imgURL;
-
+                
                 const recipeDiv = document.createElement("p");
                 recipeDiv.setAttribute("id", "instructions");
                 recipeDiv.innerHTML = recipeInstructions;
-
-                drinkDiv.append(cockTailDiv);
-                recipeContainerDiv.append(recipeDiv);
-                imageContainerDiv.append(imageDiv);
-
-                document.getElementById("drink-names").prepend(cockTailDiv,imageDiv, recipeDiv);            
+                nameRecipeWrapper.append(cockTailDiv)
+                nameRecipeWrapper.append(recipeDiv)
+                wrapper.append(nameRecipeWrapper)
+                //wrapper.append(recipeDiv)
+                wrapper.append(imageDiv);
+                
+                document.getElementById("drink-names").prepend(wrapper);            
             }
         });
     });

0f3b4b6893c9d5eee6c7e9fede29dba996eff310
Anthony Dominguez <Anthonydv7@outlook.com>
2019-09-04 11:32:14 -0500

    saving a version before i merge it with the rest of the group

--- assets/javascript/FirstFewButtons.js
+++ assets/javascript/FirstFewButtons.js
@@ -1,55 +1,92 @@
 
 const getIngredients = obj => {
+
     const re = 'strIngredient';
 
     const keys = Object.keys(obj).filter(key => key.indexOf(re) > -1);
-    return keys.map(key => obj[key]).filter(ingred => ingred !== '');
+
+    const filter =  keys.map(key => obj[key]).filter(ingred => ingred !== '' && ingred !== " ");
+
+    const ingredientsList = document.createElement("UL");
+
+    for (let i =0; i<filter.length; i++){
+
+        const filteredList = filter[i];
+
+        const filteredListItem = document.createElement("li");
+
+        filteredListItem .textContent = filteredList;
+
+        ingredientsList.append(filteredListItem)
+
+    }
+
+    return ingredientsList;
 
 }
-document.querySelectorAll("button").forEach(function (node) {
 
-    node.addEventListener("click", function () {
+
+document.querySelectorAll("button").forEach(node => {
+
+    node.addEventListener("click", () => {
 
         const drink = event.target.id;
 
         const cockTailQueryUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink;
 
-        fetch(cockTailQueryUrl).then(function (response) {
+        fetch(cockTailQueryUrl).then(response => {
             return response.json()
-        }).then(function (responseJson) {
+        }).then(responseJson => {
 
             const results = responseJson;
+
             document.getElementById("drink-names").innerHTML = '';
-        
+
             let wrapper;
+
             for (let i = 0; i < 3; i++) {
-                const ingredients = getIngredients(results.drinks[i]);
-                console.log(ingredients);
+                
+             
+
                 wrapper = document.createElement('div');
                 wrapper.classList.add('drink-wrapper');
-                const cockTail = results.drinks[i].strDrink;
+
+                const cockTailName = results.drinks[i].strDrink;
                 const recipeInstructions = results.drinks[i].strInstructions;
                 const imgURL = responseJson.drinks[i].strDrinkThumb;
+
                 const nameRecipeWrapper = document.createElement('div');
                 nameRecipeWrapper.classList.add('name-recipe-wrapper');
+
                 const cockTailDiv = document.createElement("p");
                 cockTailDiv.setAttribute("id", "name");
-                cockTailDiv.innerHTML = cockTail;
-                
+                cockTailDiv.innerHTML = cockTailName;
+
                 const imageDiv = document.createElement("img");
                 imageDiv.setAttribute("src", imgURL);
                 imageDiv.innerHTML = imgURL;
-                
+
                 const recipeDiv = document.createElement("p");
                 recipeDiv.setAttribute("id", "instructions");
                 recipeDiv.innerHTML = recipeInstructions;
-                nameRecipeWrapper.append(cockTailDiv)
-                nameRecipeWrapper.append(recipeDiv)
-                wrapper.append(nameRecipeWrapper)
-                //wrapper.append(recipeDiv)
-                wrapper.append(imageDiv);
+
+                const ingredientsDiv = document.createElement("p");
+                ingredientsDiv.setAttribute("id", "ingredients-list");
+
+                const ingredients = getIngredients(results.drinks[i]);
+                console.log(ingredients);
+
+                const ingredientsList = document.createElement("UL");
+                ingredientsList.innerHTML = ingredients;
+
                 
-                document.getElementById("drink-names").prepend(wrapper);            
+
+                ingredientsDiv.append(ingredients);
+                nameRecipeWrapper.append(cockTailDiv, ingredientsDiv);
+                wrapper.append(nameRecipeWrapper, recipeDiv, imageDiv);
+
+                document.getElementById("drink-names").prepend(wrapper);
+
             }
         });
     });
